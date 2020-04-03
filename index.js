1a4195a7403a138d481d69a8961e38808604c9cf
bahriddin <bahriddin.abdiev@ferocia.com.au>
2020-04-03 17:58:19 +1100

    get suburb data from csv

--- index.js
+++ index.js
@@ -3,17 +3,40 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
-// read all geojson data
-let rawdata = fs.readFileSync('territories/act.geojson');
-let geojson = JSON.parse(rawdata);
-console.log(geojson);
+let input_path = 'inputs/';
 
-// let areas = [];
-// fs.createReadStream('postcode-dataout.csv')
-//   .pipe(csv())
-//   .on('data', (row) => {
-//     areas.push(row);
-//   })
-//   .on('end', () => {
-//     console.log('CSV file successfully processed');
-//   });
+// read all geojson data
+let rawdata = fs.readFileSync(input_path + 'territories/act.geojson');
+let geojson = JSON.parse(rawdata);
+
+// read csv suburbs data
+let areas = [];
+fs.createReadStream(input_path + 'postcode-dataout.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    areas.push(row);
+  })
+  .on('end', () => {
+    
+    function getSuburb(state, suburbName){
+      let found_suburb = areas.find(function(area) {
+        return area.state == state && area.suburb == suburbName;
+      });
+    
+      return found_suburb;
+    }
+    
+    for(let i = 0; i < geojson.features.length; i++) {
+      let suburbName = geojson.features[i].properties['act_loca_2'];
+      // console.log(getSuburb('ACT', suburbName));
+      if(getSuburb('ACT', suburbName) == undefined) {
+        console.log('ACT', suburbName);
+      }
+      
+      // geojson.features[i].properties.postcode = 
+      // geojson.features[i].properties.state = 
+    }
+
+  });
+
+
