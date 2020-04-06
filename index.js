1535530486ab6df5516dd8557a9fed3b327a8a8a
bahriddin <bahriddin.abdiev@ferocia.com.au>
2020-04-06 11:44:49 +1000

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 let input_path = 'inputs/';
 
 // read all geojson data
-let rawdata = fs.readFileSync(input_path + 'territories/act.geojson');
+let rawdata = fs.readFileSync(input_path + 'territories/act.json');
 let geojson = JSON.parse(rawdata);
 
 // read csv suburbs data
@@ -28,13 +28,12 @@ fs.createReadStream(input_path + 'postcode-dataout.csv')
     
     for(let i = 0; i < geojson.features.length; i++) {
       let suburbName = geojson.features[i].properties['act_loca_2'];
-      // console.log(getSuburb('ACT', suburbName));
-      if(getSuburb('ACT', suburbName) == undefined) {
-        console.log('ACT', suburbName);
-      }
+      let suburbData = getSuburb('ACT', suburbName);
+      geojson.features[i].properties.postcode = suburbData == undefined ? '' : suburbData.postcode;
+      geojson.features[i].properties.state = 'ACT';
+      geojson.features[i].properties.suburb = suburbName;
       
-      // geojson.features[i].properties.postcode = 
-      // geojson.features[i].properties.state = 
+      fs.writeFileSync('outputs/act.geojson', JSON.stringify(geojson));
     }
 
   });
