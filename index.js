299d707e0c9e6c2f5ee098247e24c8e607d77164
bahriddin <bahriddin.abdiev@ferocia.com.au>
2020-04-06 21:48:53 +1000

    shrink and clean geojson files

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const fs = require('fs');
 let input_path = 'inputs/';
 
 // read all geojson data
-let rawdata = fs.readFileSync(input_path + 'territories/act.json');
+let state = 'wa';
+let filename = state + '-2';
+let rawdata = fs.readFileSync(input_path + 'territories/' + filename + '.json');
 let geojson = JSON.parse(rawdata);
 
 // read csv suburbs data
@@ -27,13 +29,15 @@ fs.createReadStream(input_path + 'postcode-dataout.csv')
     }
     
     for(let i = 0; i < geojson.features.length; i++) {
-      let suburbName = geojson.features[i].properties['act_loca_2'];
-      let suburbData = getSuburb('ACT', suburbName);
+      let suburbName = geojson.features[i].properties[state + '_local_2'];
+      // console.log(suburbName);
+      let suburbData = getSuburb(state.toUpperCase(), suburbName);
+      geojson.features[i].properties = {};
+      geojson.features[i].properties.state = state.toUpperCase();
       geojson.features[i].properties.postcode = suburbData == undefined ? '' : suburbData.postcode;
-      geojson.features[i].properties.state = 'ACT';
       geojson.features[i].properties.suburb = suburbName;
       
-      fs.writeFileSync('outputs/act.geojson', JSON.stringify(geojson));
+      fs.writeFileSync(input_path + 'territories/' + filename + '-clean.geojson', JSON.stringify(geojson));
     }
 
   });
