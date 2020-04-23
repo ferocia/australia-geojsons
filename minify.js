961752ab8f7a0248898b882d253a9b333df59b2b
bahriddin <bahriddin.abdiev@ferocia.com.au>
2020-04-23 15:51:19 +1000

    minify output

--- minify.js
+++ minify.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const fs = require('fs');
-let filePath = 'inputs/postal_areas_2011/au-postcodes-Visvalingam-5';
+let filePath = 'outputs/au-postcodes-Visvalingam-5';
 let rawdata = fs.readFileSync(filePath + '.geojson');
 let geojson = JSON.parse(rawdata);
+
+for(let i = 0; i < geojson.features.length; i++) {
+  let postalArea = geojson.features[i];
+  if(Object.keys(postalArea.properties).length !== 3 || 
+      !postalArea.properties.hasOwnProperty("POA_CODE") || 
+      !postalArea.properties.hasOwnProperty("POA_NAME") || 
+      !postalArea.properties.hasOwnProperty("SQKM")) {
+    console.log("something is wrong here");
+  }
+}
 
 fs.writeFileSync(filePath + '.min.geojson', JSON.stringify(geojson));
