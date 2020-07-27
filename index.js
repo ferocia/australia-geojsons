b1cd5941fc677dc38f1482d610f286f8256a53f3
bahriddin <bahriddin.abdiev@ferocia.com.au>
2020-07-27 15:17:57 +1000

    Add few vic suburb postcodes

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const fs = require('fs');
 let input_path = 'inputs/';
 
 // read all geojson data
-let state = 'tas';
-let filename = state + '-5';
-let rawdata = fs.readFileSync(input_path + 'territories/' + filename + '.json');
+let state = 'vic';
+let filename = state + '-5'; // this is to read from 5% accurate file
+let rawdata = fs.readFileSync(input_path + 'territories/' + filename + '.geojson');
 let geojson = JSON.parse(rawdata);
 
 // read csv suburbs data
@@ -19,15 +19,15 @@ fs.createReadStream(input_path + 'postcode-dataout.csv')
     areas.push(row);
   })
   .on('end', () => {
-    
+
     function getSuburb(state, suburbName){
       let found_suburb = areas.find(function(area) {
         return area.state == state && area.suburb == suburbName;
       });
-    
+
       return found_suburb;
     }
-    
+
     for(let i = 0; i < geojson.features.length; i++) {
       let suburbName = geojson.features[i].properties[state + '_loca_2'];
       // console.log(suburbName);
@@ -36,7 +36,9 @@ fs.createReadStream(input_path + 'postcode-dataout.csv')
       geojson.features[i].properties.state = state.toUpperCase();
       geojson.features[i].properties.suburb = suburbName;
       geojson.features[i].properties.postcode = suburbData == undefined ? '' : suburbData.postcode;
-      
+      if(suburbData == undefined ) {
+        console.log("Couldn't find postcode for suburb:", suburbName);
+      }
     }
 
     fs.writeFileSync(input_path + 'territories/' + filename + '-clean.geojson', JSON.stringify(geojson, null, 2));
